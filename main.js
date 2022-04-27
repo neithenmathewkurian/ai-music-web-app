edfa5464e5234fa7eb0aefecbb25b2d732089554
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2022-04-27 13:53:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+
+function setup(){
+    canvas=createCanvas(300,220);
+    canvas.position(300,175);
+    video=createCapture(VIDEO);
+     video.hide();
+}
+
+
+function draw(){
+
+}

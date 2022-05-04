9e93094fde0e7618461d0ed9c20d1bf894b855e4
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2022-05-04 19:21:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function preload(){
 
 
 function setup(){
-    canvas=createCanvas(300,220);
-    canvas.position(300,175);
+    canvas=createCanvas(400,320);
+    canvas.position(400,200);
     video=createCapture(VIDEO);
      video.hide();
 }
 
 
 function draw(){
-
+image(video,0,0,300,220)
 }
